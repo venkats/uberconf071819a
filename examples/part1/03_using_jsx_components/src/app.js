7f594002bc1e23e4dae411c9de516679cec3bb62
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-18 14:23:07 -0600

    using jsx components

---
+++ examples/part1/03_using_jsx_components/src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return <div>
+      <h1>This is a test</h1>
+      <p>This is a line</p>
+    </div>;
+  }
+}
